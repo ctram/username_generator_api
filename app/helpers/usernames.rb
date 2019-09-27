86e5b2f9eb39055388ba4605243d103d3c93dc34
Chris Tram <ctram@protonmail.com>
2019-09-26 18:02:03 -0700

    conditionally format the username

--- app/helpers/usernames.rb
+++ app/helpers/usernames.rb
@@ -1,8 +1,22 @@
 module Usernames
-  def self.generate
+  def self.generate(options = { case: 'snake' })
     base_path = File.join(Dir.pwd, 'lib')
     adjectives = File.open(base_path + '/adjectives.txt', 'r').readlines(chomp: true)
     nouns = File.open(base_path + '/nouns.txt', 'r').readlines(chomp: true)
-    "#{adjectives.sample} #{nouns.sample}"
+
+    words = "#{adjectives.sample} #{nouns.sample}".split(' ')
+
+    case options[:case].to_sym
+    when :camel
+      words.map{ |w| w.capitalize }.join('')
+    when :snake
+      words.join('_')
+    when :hyphen
+      words.join('-')
+    when :space
+      words.join(' ')
+    else
+      words
+    end
   end
 end
